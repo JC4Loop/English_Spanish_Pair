524c4edbd40fde91b464a6f2979f08d23083837d
JustinWin10 <juicetinc4@gmail.com>
2019-07-23 13:13:06 +0000

    Changed Component class to external MovingWord class, fixed gitignore for pycache

--- EspEngPair1.0.py
+++ EspEngPair1.0.py
@@ -1,111 +1,9 @@
 from tkinter import*
+from MovingWord import *
 import time
 from random import shuffle
 from pairDb import*
 
-
-class Component:
-	# try the canvas element as Class var instead of instance var
-	gID = 0
-	canvas = None
-
-	def __init__(self,x,y,txt,pid,lang):
-		padding = 0
-		if len(txt) > 8:
-			moreThan8 = len(txt) - 8
-			for i in range(0,moreThan8):
-				padding += 10
-
-		self.cID = Component.gID
-		Component.gID += 1
-		if lang == "eng":
-			color = "white"
-		else:
-			color = "#ffffb3" #light yellow
-		self.pairID = pid
-		self.x = x
-		self.y = y
-		self.width = 100 + padding
-		self.height = 50
-		self.rectangle = Component.canvas.create_rectangle(x,y,x + self.width , y + self.height ,fill = color,activefill='cyan')
-		self.Selected = False
-		self.text = txt
-		self.txtObj = Component.canvas.create_text(x + 45 + (padding /2), y + 20, font=("Purisa", 12), text = txt,fill = "blue")
-		self.mX = 0
-		self.mY = 0
-		self.waitingToMove = False
-		self.toBeRemoved = False
-
-	@staticmethod
-	def initCanvas(c):
-		Component.canvas = c;
-
-	def getRectangle(self):
-		return self.rectangle
-	def getTxtObj(self):
-		return self.txtObj
-	def getX(self):
-		return self.x
-	def getY(self):
-		return self.y
-	def getMX(self):
-		return self.mX
-	def setMX(self,v):
-		self.mX = v
-	def getMY(self):
-		return self.mY
-	def setMY(self,v):
-		self.mY = v
-
-	def getLeft(self):
-		return self.x
-	def getRight(self):
-		return self.x + self.width
-	def getTop(self):
-		return self.y
-	def getBottom(self):
-		return self.y + self.height
-
-	def moveComp(self,a,b):
-		self.x += a
-		self.y += b
-		c.move(self.rectangle,a,b)
-		c.move(self.txtObj,a,b)
-
-
-	def canMoveLeft(self,otherS):
-		bCanMoveL = True
-		gap = 20
-		for i in range(0,len(otherS)):
-			bCanMoveL = True
-			if otherS[i].cID == self.cID: 
-				continue
-			if self.getLeft() <= otherS[i].getRight() + gap and not(self.getBottom() <= otherS[i].getTop() or self.getTop() >= otherS[i].getBottom()):
-				if otherS[i].getLeft() > self.getRight():
-					continue
-				else:
-					bCanMoveL = False
-					break
-	
-		return bCanMoveL
-
-	def collision(self,otherS):
-		bCollision = True
-		for i in range(0,len(otherS)):
-			bCollision = True
-			gap = 20
-			if otherS[i].cID == self.cID: # dont check for collision with itself
-				bCollision = False
-				continue
-			if self.getLeft() >=  otherS[i].getRight() + gap or self.getRight() <= otherS[i].getLeft() - gap \
-				or self.getBottom() <= otherS[i].getTop() - gap or self.getTop() >= otherS[i].getBottom() + gap:
-				bCollision = False
-			if bCollision == True:
-				break
-	
-		return bCollision
-
-#---------------------------------------------------END of Component Class
 
 def removeFromActiveComps(cID):
 	for i in range(len(activeComponents)-1,-1,-1):
@@ -189,7 +87,7 @@ c = Canvas(gui, width=800 ,height=800)
 c.pack()
 c.create_rectangle(0,0,800,800, fill="black")
 c.create_image((500, 500), image = small_img)
-Component.initCanvas(c)
+MovingWord.initCanvas(c)
 
 selectedCs = []
 activeComponents = []
@@ -237,7 +135,7 @@ def placeOnCanvas(preparePairs, maxOnScreen,first,last,numLeft):
 	x ,y = 500, 100
 	j = 0
 	for i in range(first,last):
-		activeComponents.append(Component(x,y,preparePairs[j].word, preparePairs[j].pairID,preparePairs[j].lang))
+		activeComponents.append(MovingWord(x,y,preparePairs[j].word, preparePairs[j].pairID,preparePairs[j].lang))
 		activeComponents[i].setMX(-1)
 		j += 1
 		y += 150
@@ -286,7 +184,9 @@ while True:
 		frameCount += 1
 		gui.update_idletasks()
 		gui.update()
+		print('after gui.update()')
 		movementCalc(frameCount)
+		print('after movementCalc')
 	except:
 		print("Caught gui.update() error") # error only occurs on close
 		break
